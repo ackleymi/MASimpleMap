4fc3fb2410bc3812519cdf8878c557f144e8de11
ackleymi <ackleymi@gmail.com>
2014-03-22 21:34:57 -0400

    Initial Commit

---
+++ MASimpleMap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MASimpleMap
+//
+//  Created by Michael Ackley on 3/22/14.
+//  Copyright (c) 2014 Michael Ackley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MASimpleMap/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MASimpleMap
+//
+//  Created by Michael Ackley on 3/22/14.
+//  Copyright (c) 2014 Michael Ackley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
